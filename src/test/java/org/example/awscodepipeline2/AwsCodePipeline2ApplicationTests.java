971b7c9387e8b3a32a0cbf187657517735afc305
thechamplive <sohaanmetai@gmail.com>
2024-08-07 10:08:32 -0500

    first commit

---
+++ src/test/java/org/example/awscodepipeline2/AwsCodePipeline2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awscodepipeline2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodePipeline2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
